be81e2fc06d9b5ecc43865c476451412d1335b48
Hosi121 <takuyakataiwa@icloud.com>
2024-09-12 23:39:30 +0900

    feat: apiを定義

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Box, Button, Container, TextField, Typography, IconButton, InputAdornment } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
--- frontend/src/components/pages/SignUp.tsx
+++ frontend/src/components/pages/SignUp.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Box, Button, Container, TextField, Typography, Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
