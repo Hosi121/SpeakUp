d22aad809130fc1364bdabc90db17a41682199f7
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-09-12 23:49:21 +0900

    feat: bottom navigationからページ遷移できるようにした

--- frontend/src/components/commons/MainBottomNavigation.tsx
+++ frontend/src/components/commons/MainBottomNavigation.tsx
@@ -1,19 +1,21 @@
 import { Home, LibraryBooks, Mic } from "@mui/icons-material";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 type bottomNavigationProps = {
     value: number;
 }
 
 export const MainBottomNavigation = ({ value }: bottomNavigationProps) => {
+    const navigate = useNavigate();
     return (
         <BottomNavigation
             showLabels
             value={value}
         >
-            <BottomNavigationAction icon={<LibraryBooks />} />
-            <BottomNavigationAction icon={<Home />} />
-            <BottomNavigationAction icon={<Mic />} />
+            <BottomNavigationAction onClick={() => navigate("/record")} icon={<LibraryBooks />} />
+            <BottomNavigationAction onClick={() => navigate("/home")} icon={<Home />} />
+            <BottomNavigationAction onClick={() => navigate("/session")} icon={<Mic />} />
         </BottomNavigation>
     )
 }
--- frontend/src/components/commons/SessionBottomNavigation.tsx
+++ frontend/src/components/commons/SessionBottomNavigation.tsx
@@ -1,21 +1,23 @@
 import { Message, Mic, PriorityHigh, Search } from "@mui/icons-material";
 import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 type sessionBottomNavigation = {
     value: number;
 }
 
 export const SessionBottomNavigation = ({ value }: sessionBottomNavigation) => {
+    const navigate = useNavigate();
     return (
         <Box>
             <BottomNavigation
                 showLabels
                 value={value}
             >
-                <BottomNavigationAction icon={<Mic />} />
-                <BottomNavigationAction icon={<Message />} />
-                <BottomNavigationAction icon={<Search />} />
-                <BottomNavigationAction icon={<PriorityHigh />} />
+                <BottomNavigationAction onClick={() => navigate("")} icon={<Mic />} />
+                <BottomNavigationAction onClick={() => navigate("")} icon={<Message />} />
+                <BottomNavigationAction onClick={() => navigate("")} icon={<Search />} />
+                <BottomNavigationAction onClick={() => navigate("")} icon={<PriorityHigh />} />
             </BottomNavigation>
         </Box>
     )
