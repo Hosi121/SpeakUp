a9a0de6ec838512959462bb9955a14cc37e41749
Hosi121 <takuyakataiwa@icloud.com>
2024-09-14 01:00:19 +0900

    feat : corsを追加

---
+++ backend/middlewares/cors_middleware.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    "time"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+    return cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173"}, // フロントエンドのURLを指定
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    })
+}
