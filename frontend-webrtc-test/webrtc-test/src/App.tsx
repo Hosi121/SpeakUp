c1af3cd2096b27900ca557a391ff63b23ec3aaee
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-09-14 00:07:03 +0900

    feat: webカメラの映像を表示できるようになった

--- frontend-webrtc-test/webrtc-test/src/App.tsx
+++ frontend-webrtc-test/webrtc-test/src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { connect, hangUp, onSdpText, startVideo } from "./webrtc";
+import styles from "./App.module.css";
+import React, { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then(function (stream) {
+      // success
+      console.log(stream);
+    })
+    .catch(function (error) {
+      // error
+      console.log(error);
+      return;
+    });
+
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
 
   return (
-    <>
+    <div>
+      WebRTC test
+      <br />
+      <button type="button" onClick={() => startVideo(localVideoRef)}>
+        Start Video
+      </button>
+      <button type="button" onClick={() => connect()}>
+        Connect
+      </button>
+      <button type="button" onClick={() => hangUp()}>
+        Hang Up
+      </button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <video
+          className={styles.video}
+          ref={localVideoRef}
+          id="local_video"
+          autoPlay
+          muted
+        ></video>
+        <video
+          className={styles.video}
+          ref={remoteVideoRef}
+          id="remote_video"
+          autoPlay
+        ></video>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        SDP to send:
+        <br />
+        <textarea className={styles.textArea} id="text_for_send_sdp" readOnly>
+          SDP to send
+        </textarea>
       </p>
-    </>
-  )
+      <p>
+        SDP to receive:
+        <button type="button" onClick={() => onSdpText()}>
+          Receive remote SDP
+        </button>
+        <br />
+        <textarea
+          className={styles.textArea}
+          id="text_for_receive_sdp"
+        ></textarea>
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
