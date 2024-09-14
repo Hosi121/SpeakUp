934acef383f47fa16f9063f8994b008eb7e650a1
koto623 <akan.02toko@gmail.com>
2024-09-14 21:06:23 +0900

    style: 色とサイズの変更

--- frontend/src/components/pages/Waiting.tsx
+++ frontend/src/components/pages/Waiting.tsx
@@ -1,30 +1,26 @@
-import { Box, Button, Container, Typography } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
-import Memo from './Memo';
+import { Box, Button, Container, Typography } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import Memo from "./Memo";
 
 const Waiting = () => {
   return (
-    <Container sx={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 3 }}>
-      <Typography variant="h6" sx={{ mb: 2, backgroundColor: '#FFD700', borderRadius: 1, padding: 1, textAlign: 'center' }}>
+    <Container sx={{ height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: 3 }}>
+      <Typography variant="h6" sx={{ mb: 2, width: "70%", maxWidth: "300px", backgroundColor: "secondary.main", color: "primary.main", borderRadius: 1, padding: 1, textAlign: "center" }}>
         6月30日21:00〜
       </Typography>
 
-      <Box sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper', borderRadius: 1, p: 2, boxShadow: 3 }}>
+      <Box sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper", borderRadius: 1, p: 2, boxShadow: 3 }}>
         <Typography variant="subtitle1" gutterBottom component="div">
           持ち込みメモ
         </Typography>
         <Memo />
       </Box>
 
-      <Box sx={{ mt: 2, display: 'flex', justifyContent: 'center', width: '100%' }}>
-        <CircularProgress size={24} />
-        <Typography variant="caption" sx={{ ml: 1 }}>
-          Now loading...
-        </Typography>
-      </Box>
-
-      <Button variant="contained" sx={{ mt: 4, bgcolor: 'secondary.main', '&:hover': { bgcolor: 'secondary.dark' } }}>
-        セッション開始まで<br/>もうしばらくお待ちください
+      <Button variant="contained" sx={{ mt: 4, color: "#000", bgcolor: "secondary.main", "&:hover": { bgcolor: "secondary.dark" }, width: "70%", maxWidth: "300px" }}>
+        <CircularProgress size={24} sx={{ mr: 3 }} />
+        セッション開始まで
+        <br />
+        しばらくお待ちください
       </Button>
     </Container>
   );
