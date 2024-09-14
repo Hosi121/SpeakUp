5c52f53d7b40037eff9ae627587308002fbe3f43
koto623 <akan.02toko@gmail.com>
2024-09-14 21:47:07 +0900

    feat: TopSectionとBottomNavigationを追加

--- frontend/src/components/pages/Record.tsx
+++ frontend/src/components/pages/Record.tsx
@@ -1,23 +1,31 @@
 import { Stack } from "@mui/system";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import DescriptionIcon from "@mui/icons-material/Description";
 import FolderOutlinedIcon from "@mui/icons-material/FolderOutlined";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 import { IconButton } from "../utils/IconButton";
+import TopSection from "../utils/TopSection";
+import { MainBottomNavigation } from "../utils/MainBottomNavigation";
 
 export const Record = () => {
   return (
-    <Stack sx={{ margin: "0 auto", width: "90%", p: 3 }}>
-      <Typography align="left" sx={{ marginBottom: "30px", fontSize: "2rem", color: "primary.main" }}>
-        記録
-      </Typography>
-      <Box sx={{ display: "flex", placeContent: "center", flexWrap: "wrap", justifyContent: "space-between" }}>
-        <IconButton icon={<DescriptionIcon sx={{ fontSize: "60px" }} />} text="会話の記録" url="conversationrecords" />
-        <IconButton icon={<FolderOutlinedIcon sx={{ fontSize: "60px" }} />} text="履歴" url="history" />
-        <IconButton icon={<EmojiEventsIcon sx={{ fontSize: "60px" }} />} text="データ" url="stats" />
-        <IconButton icon={<EditNoteIcon sx={{ fontSize: "60px" }} />} text="持ち込みメモ" url="memo" />
-      </Box>
-    </Stack>
+    <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100vh" }}>
+      <Container sx={{ p: 3 }}>
+        <TopSection />
+        <Stack sx={{ margin: "30px auto 0", width: "90%" }}>
+          <Typography align="left" sx={{ marginBottom: "30px", fontSize: "2rem", color: "primary.main" }}>
+            記録
+          </Typography>
+          <Box sx={{ display: "flex", placeContent: "center", flexWrap: "wrap", justifyContent: "space-between" }}>
+            <IconButton icon={<DescriptionIcon sx={{ fontSize: "60px" }} />} text="会話の記録" url="conversationrecords" />
+            <IconButton icon={<FolderOutlinedIcon sx={{ fontSize: "60px" }} />} text="履歴" url="history" />
+            <IconButton icon={<EmojiEventsIcon sx={{ fontSize: "60px" }} />} text="データ" url="stats" />
+            <IconButton icon={<EditNoteIcon sx={{ fontSize: "60px" }} />} text="持ち込みメモ" url="memo" />
+          </Box>
+        </Stack>
+      </Container>
+      <MainBottomNavigation value="record" />
+    </Container>
   );
 };
