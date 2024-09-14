fa3baea405a30ff009a7db90442085a587a43d6a
koto623 <akan.02toko@gmail.com>
2024-09-14 18:47:49 +0900

    style: カラーテーマを設定

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx'
-import Settings from './components/pages/Settings.tsx';
-import { Home } from './components/pages/Home.tsx';
-import { SessionList } from './components/pages/SessionList.tsx';
-import Waiting from './components/pages/Waiting.tsx';
-import { MicCheck } from './components/pages/MicCheck.tsx';
-import { Session } from './components/pages/Session.tsx';
-import { SessionInterval } from './components/pages/SessionInterval.tsx';
-import { SessionFinished } from './components/pages/SessionFinished.tsx';
-import SignUp from './components/pages/SignUp.tsx';
-import { Record } from './components/pages/Record.tsx';
-import Memo from './components/pages/Memo.tsx';
-import Message from './components/pages/Message.tsx';
-import Login from './components/pages/Login.tsx';
-import Stats from './components/pages/Stats.tsx';
-import ConversationHistory from './components/pages/ConversationHistory.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import Settings from "./components/pages/Settings.tsx";
+import { Home } from "./components/pages/Home.tsx";
+import { SessionList } from "./components/pages/SessionList.tsx";
+import Waiting from "./components/pages/Waiting.tsx";
+import { MicCheck } from "./components/pages/MicCheck.tsx";
+import { Session } from "./components/pages/Session.tsx";
+import { SessionInterval } from "./components/pages/SessionInterval.tsx";
+import { SessionFinished } from "./components/pages/SessionFinished.tsx";
+import SignUp from "./components/pages/SignUp.tsx";
+import { Record } from "./components/pages/Record.tsx";
+import Memo from "./components/pages/Memo.tsx";
+import Message from "./components/pages/Message.tsx";
+import Login from "./components/pages/Login.tsx";
+import Stats from "./components/pages/Stats.tsx";
+import ConversationHistory from "./components/pages/ConversationHistory.tsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Theme from "./styles/Theme.tsx";
 
 const router = createBrowserRouter([
   {
@@ -82,14 +84,17 @@ const router = createBrowserRouter([
   {
     path: "conversation_history",
     element: <ConversationHistory />,
-  }
+  },
 ]);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={Theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </StrictMode>
   );
 } else {
