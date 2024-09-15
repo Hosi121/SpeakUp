417a0340a5ccab9ecc596025bfafb643c30d609f
ei1906 <whitegorilla1906@gmail.com>
2024-09-15 17:21:22 +0900

    feat: トロフィーに関するentのファイルを作成

---
+++ backend/ent/schema/achievement.go
@@ -0,0 +1,17 @@
+package schema
+
+import (
+	"entgo.io/ent"
+)
+
+type ACHIEVEMENT struct {
+	ent.Schema
+}
+
+func (ACHIEVEMENT) Fields() []ent.Field {
+	return []ent.Field{}
+}
+
+func (AITHEMES) Edges() []ent.Edge {
+	return []ent.Edge{}
+}
---
+++ backend/ent/schema/trophy.go
@@ -0,0 +1,17 @@
+package schema
+
+import (
+	"entgo.io/ent"
+)
+
+type TROPHY struct {
+	ent.Schema
+}
+
+func (TROPHY) Fields() []ent.Field {
+	return []ent.Field{}
+}
+
+func (TROPHY) Edges() []ent.Edge {
+	return []ent.Edge{}
+}
