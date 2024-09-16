414fee16490edd4e6ffef79857f5e759eda9bc9c
koto623 <akan.02toko@gmail.com>
2024-09-16 20:31:18 +0900

    feat: ロゴ画像を表示

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,8 +1,24 @@
-import { Stack } from "@mui/system";
+import { Box, Container } from "@mui/material";
 import { BottomNavigationTemplate } from "../templates/BottomNavigationTemplate";
+import TopSection from "../utils/TopSection";
+import TopWaves from "../utils/TopWaves";
+import HomeLogo from "../../assets/homeLogo";
 
 const HomeContainer = () => {
-  return <Stack>home</Stack>;
+  return (
+    <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "calc(100vh - 70px)" }}>
+      <Container sx={{ pt: 3 }}>
+        <TopSection />
+        <Container sx={{ mb: 3 }}>
+          <TopWaves isFlipped={false} />
+          <Box sx={{ margin: "0 calc(50% - 50vw)", width: "100vw", height: "15vh", backgroundColor: "secondary.main", display: "grid", placeItems: "center" }}>
+            <HomeLogo style={{ width: "70%" }} />
+          </Box>
+          <TopWaves isFlipped={true} />
+        </Container>
+      </Container>
+    </Container>
+  );
 };
 
 export const Home = () => {
