e54f5f4ecf9a08885e529fdb54a57f795d7a1f7c
koto623 <akan.02toko@gmail.com>
2024-09-16 20:51:32 +0900

    feat: iconButtonの追加と直近の参加予定の追加

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,8 +1,10 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, List, ListItem, ListItemText } from "@mui/material";
 import { BottomNavigationTemplate } from "../templates/BottomNavigationTemplate";
 import TopSection from "../utils/TopSection";
 import TopWaves from "../utils/TopWaves";
 import HomeLogo from "../../assets/homeLogo";
+import { IconButton } from "../utils/IconButton";
+import { LibraryBooks, Mic } from "@mui/icons-material";
 
 const HomeContainer = () => {
   return (
@@ -16,6 +18,34 @@ const HomeContainer = () => {
           </Box>
           <TopWaves isFlipped={true} />
         </Container>
+        <Box sx={{ width: "100%", backgroundColor: "secondary.main", display: "flex", flexDirection: "column", alignItems: "center", mt: 3, mb: 3, p: 3, boxSizing: "border-box", borderRadius: 3 }}>
+          <h2>直近の参加予定</h2>
+          <List>
+            <ListItem>
+              <ListItemText primary="6月30日21:00~" primaryTypographyProps={{ color: "primary.main", fontSize: "1.3rem", fontWeight: "bolder", textAlign: "center" }} />
+            </ListItem>
+            <ListItem sx={{ p: 0 }}>
+              <ListItemText primary="セッション1" primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
+            </ListItem>
+            <ListItem sx={{ p: 0 }}>
+              <ListItemText primary="セッション2" primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
+            </ListItem>
+            <ListItem sx={{ p: 0 }}>
+              <ListItemText primary="セッション3" primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
+            </ListItem>
+          </List>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            placeContent: "center",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          <IconButton icon={<LibraryBooks sx={{ fontSize: "60px" }} />} text="記録" url="record" />
+          <IconButton icon={<Mic sx={{ fontSize: "60px" }} />} text="セッション" url="sessionlist" />
+        </Box>
       </Container>
     </Container>
   );
