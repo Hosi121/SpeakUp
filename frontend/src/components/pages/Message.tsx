104b815c5391ff8f5a18adf94be9b9755db9b2d9
koto623 <akan.02toko@gmail.com>
2024-09-16 16:44:08 +0900

    chore: サイズの調整

--- frontend/src/components/pages/Message.tsx
+++ frontend/src/components/pages/Message.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { Box, List, ListItem, ListItemText, Paper, TextField, Button, IconButton } from "@mui/material";
+import { Box, List, ListItem, ListItemText, Paper, TextField, Button, IconButton, Container } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
-import { MainBottomNavigation } from "../utils/MainBottomNavigation";
 import TopSection from "../utils/TopSection"; // Import TopSection component
 import { BottomNavigationTemplate } from "../templates/BottomNavigationTemplate";
 
@@ -25,31 +24,37 @@ const MessageContainer = () => {
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "space-between" }}>
-      {/* Back Button at the top-left corner */}
-      <Box sx={{ position: "relative", display: "flex", justifyContent: "flex-start", p: 2 }}>
-        <IconButton onClick={handleGoBack}>
-          <ArrowBackIcon sx={{ fontSize: 40 }} />
-        </IconButton>
-      </Box>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "calc(100vh - 70px)",
+      }}
+    >
+      <Container sx={{ pt: 3 }}>
+        <TopSection />
+        {/* Back Button at the top-left corner */}
+        <Box sx={{ position: "relative", display: "flex", justifyContent: "flex-start", pt: 2, pb: 2 }}>
+          <IconButton onClick={handleGoBack}>
+            <ArrowBackIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        </Box>
 
-      {/* Use the TopSection component here */}
-      <TopSection />
-
-      {/* Chat Messages */}
-      <Box sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
-        <List>
-          {/* Render each message from the state */}
-          {messages.map((message, index) => (
-            <ListItem key={index} sx={{ justifyContent: "flex-end" }}>
-              <Paper sx={{ padding: "10px", backgroundColor: "#f0f0f0", maxWidth: "60%", wordWrap: "break-word" }}>
-                <ListItemText primary={message} />
-              </Paper>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-
+        {/* Chat Messages */}
+        <Box sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
+          <List>
+            {/* Render each message from the state */}
+            {messages.map((message, index) => (
+              <ListItem key={index} sx={{ justifyContent: "flex-end" }}>
+                <Paper sx={{ padding: "10px", backgroundColor: "#f0f0f0", maxWidth: "60%", wordWrap: "break-word" }}>
+                  <ListItemText primary={message} />
+                </Paper>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Container>
       {/* Message Input */}
       <Box sx={{ display: "flex", alignItems: "center", p: 2, borderTop: "1px solid #ccc" }}>
         <TextField variant="outlined" placeholder="メッセージを入力" fullWidth value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} sx={{ mr: 2 }} />
@@ -57,10 +62,7 @@ const MessageContainer = () => {
           送信
         </Button>
       </Box>
-
-      {/* Footer Navigation */}
-      <MainBottomNavigation value="home" />
-    </Box>
+    </Container>
   );
 };
 
