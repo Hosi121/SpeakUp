a028e88d74059693c539707c36c80c57a7b319a2
koto623 <akan.02toko@gmail.com>
2024-09-17 12:20:02 +0900

    refactor: モックデータをjsonに分離

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Box, Container, List, ListItem, ListItemText } from "@mui/material";
 import { BottomNavigationTemplate } from "../templates/BottomNavigationTemplate";
 import TopSection from "../utils/TopSection";
@@ -5,8 +6,20 @@ import TopWaves from "../utils/TopWaves";
 import HomeLogo from "../../assets/homeLogo";
 import { IconButton } from "../utils/IconButton";
 import { LibraryBooks, Mic } from "@mui/icons-material";
+import session from "../../mock/sessions.json";
+
+type SessionData = {
+  dateTime: string;
+  sessions: number[];
+};
 
 const HomeContainer = () => {
+  const [sessionData, setSessionData] = useState<SessionData>();
+
+  useEffect(() => {
+    setSessionData(session);
+  });
+
   return (
     <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "calc(100vh - 70px)" }}>
       <Container sx={{ pt: 3 }}>
@@ -22,17 +35,13 @@ const HomeContainer = () => {
           <h2>直近の参加予定</h2>
           <List>
             <ListItem>
-              <ListItemText primary="6月30日21:00~" primaryTypographyProps={{ color: "primary.main", fontSize: "1.3rem", fontWeight: "bolder", textAlign: "center" }} />
+              <ListItemText primary={sessionData?.dateTime} primaryTypographyProps={{ color: "primary.main", fontSize: "1.3rem", fontWeight: "bolder", textAlign: "center" }} />
             </ListItem>
-            <ListItem sx={{ p: 0 }}>
-              <ListItemText primary="セッション1" primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
-            </ListItem>
-            <ListItem sx={{ p: 0 }}>
-              <ListItemText primary="セッション2" primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
-            </ListItem>
-            <ListItem sx={{ p: 0 }}>
-              <ListItemText primary="セッション3" primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
-            </ListItem>
+            {sessionData?.sessions.map((session) => (
+              <ListItem sx={{ p: 0 }}>
+                <ListItemText primary={"セッション" + session} primaryTypographyProps={{ color: "primary.main", textAlign: "center" }} />
+              </ListItem>
+            ))}
           </List>
         </Box>
         <Box
