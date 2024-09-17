9008c48537644a37da1b095299e16f4f65fe52d7
koto623 <akan.02toko@gmail.com>
2024-09-17 15:32:28 +0900

    feat: 参加可能なセッションの追加

--- frontend/src/components/pages/SessionList.tsx
+++ frontend/src/components/pages/SessionList.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { Stack } from "@mui/system";
 import { BottomNavigationTemplate } from "../templates/BottomNavigationTemplate";
-import { Button, Container, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Checkbox, Container, FormControlLabel, FormGroup, Typography } from "@mui/material";
 import TopSection from "../utils/TopSection";
 import sessions from "../../mock/sessions.json";
 import { useNavigate } from "react-router-dom";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 type SessionData = {
   dateTime: string;
@@ -47,6 +48,51 @@ const SessionListContainer = () => {
             <Typography sx={{ mb: 2, color: "primary.main", fontSize: "1.3rem", fontWeight: "bolder", textAlign: "center" }}>{sessionData[0]?.dateTime}</Typography>
             {sessionData[0]?.sessions.map((session) => <Typography sx={{ color: "primary.main", textAlign: "center", fontSize: "0.9rem" }}>セッション{session}</Typography>)}
           </Button>
+        </Stack>
+        <Stack sx={{ margin: "30px auto 0", width: "90%" }}>
+          <Typography variant="h5" sx={{ fontWeight: "bold", textAlign: "left", mb: 3 }}>
+            参加可能なセッション
+          </Typography>
+          {sessionData.map((data, index) => (
+            <Accordion
+              key={index}
+              sx={{
+                mb: 2,
+                borderRadius: 3,
+                "&:before": {
+                  display: "none",
+                },
+              }}
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ height: "50px" }}>
+                <Typography fontWeight="bolder" fontSize="large" color="primary.main" sx={{ display: "block", m: "auto" }}>
+                  {data.dateTime}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails sx={{ p: 2 }}>
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox defaultChecked={data.sessions.includes(1)} />} label="セッション1" sx={{ m: "0 auto" }} />
+                  <FormControlLabel control={<Checkbox defaultChecked={data.sessions.includes(2)} />} label="セッション2" sx={{ m: "0 auto" }} />
+                  <FormControlLabel control={<Checkbox defaultChecked={data.sessions.includes(3)} />} label="セッション3" sx={{ m: "0 auto" }} />
+                  <Button
+                    variant="contained"
+                    fullWidth
+                    sx={{
+                      width: "50%",
+                      backgroundColor: "primary.main",
+                      color: "secondary.main",
+                      m: "20px auto",
+                      "&:hover": {
+                        backgroundColor: "#FF3399",
+                      },
+                    }}
+                  >
+                    保存
+                  </Button>
+                </FormGroup>
+              </AccordionDetails>
+            </Accordion>
+          ))}{" "}
         </Stack>
       </Container>
     </Container>
