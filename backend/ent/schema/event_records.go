dcba0dc3855b42f293f6435fc62f74ea27244569
ei1906 <whitegorilla1906@gmail.com>
2024-09-17 12:31:48 +0900

    feat: EVENT_RECORDSを新たに作成

---
+++ backend/ent/schema/event_records.go
@@ -0,0 +1,35 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// AITHEMES holds the schema definition for the AITHEMES entity.
+type EVENT_RECORDS struct {
+	ent.Schema
+}
+
+// Fields of the AITHEMES.
+func (EVENT_RECORDS) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("user_id"),
+		field.Int("event_id"),
+		field.String("records").
+			Default(""),
+	}
+}
+
+// Edges of the AITHEMES.
+func (EVENT_RECORDS) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("made", USERS.Type).
+			Ref("makes").
+			Unique(),
+		edge.From("participates", EVENTS.Type).
+			Ref("participated").
+			Unique(),
+		edge.To("has", SESSIONS.Type),
+	}
+}
