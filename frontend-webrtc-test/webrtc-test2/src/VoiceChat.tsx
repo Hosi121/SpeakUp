28f7ecbf26ca467252af7c3a167d56a361c035e6
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-09-18 11:02:01 +0900

    fix: STUNサーバを減らした

--- frontend-webrtc-test/webrtc-test2/src/VoiceChat.tsx
+++ frontend-webrtc-test/webrtc-test2/src/VoiceChat.tsx
@@ -5,9 +5,6 @@ const STUN_SERVERS = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19302" },
-    { urls: "stun:stun3.l.google.com:19302" },
-    { urls: "stun:stun4.l.google.com:19302" },
   ],
 };
 
