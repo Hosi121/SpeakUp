15c88dc3ed0848121cf087d188f88a39984bf224
ei1906 <whitegorilla1906@gmail.com>
2024-09-20 13:52:32 +0900

    add: ユーザ作成処理を追加

--- backend/mock/mock.go
+++ backend/mock/mock.go
@@ -3,6 +3,7 @@ package mock
 import (
 	"context"
 	"log"
+	"strconv"
 
 	"github.com/Hosi121/SpeakUp/ent"
 )
@@ -11,17 +12,31 @@ func CreateTestData(client *ent.Client) {
 	ctx := context.Background()
 
 	// ユーザーの作成
-	user, err := client.USERS.
+	for i := 1; i <= 30; i++ {
+		tail := strconv.Itoa(i)
+		user, err := client.USERS.
+			Create().
+			SetUsername("testuser" + tail).
+			SetEmail("testuser" + tail + "@example.com").
+			SetAvatarURL("https://example.com/avatar.png").
+			SetRole("USER").
+			Save(ctx)
+		if err != nil {
+			log.Fatalf("failed creating user: %v", err)
+		}
+
+		log.Printf("created user: %v", user)
+	}
+	admin, err := client.USERS.
 		Create().
-		SetUsername("testuser").
-		SetEmail("testuser@example.com").
-		SetHashedPassword("hashedpassword").
+		SetUsername("admin").
+		SetEmail("admin@example.com").
 		SetAvatarURL("https://example.com/avatar.png").
-		SetRole("USER").
+		SetRole("ADMIN").
 		Save(ctx)
 	if err != nil {
-		log.Fatalf("failed creating user: %v", err)
+		log.Fatalf("failed creating admin: %v", err)
 	}
+	log.Printf("created user: %v", admin)
 
-	log.Printf("created user: %v", user)
 }
