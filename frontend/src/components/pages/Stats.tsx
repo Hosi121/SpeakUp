1345379880a9a081a120d0f5f287b8ba5a5f4b62
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-09-21 13:11:04 +0900

    複数のビルドエラーを修正

--- frontend/src/components/pages/Stats.tsx
+++ frontend/src/components/pages/Stats.tsx
@@ -1,12 +1,26 @@
 import { useEffect, useState } from "react";
-import { Box, Typography, Paper, List, ListItem, ListItemIcon, ListItemText, Container, Stack } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Paper,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Container,
+  Stack,
+} from "@mui/material";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import trophyData from "../../assets/trophy.json";
 import TopSection from "../utils/TopSection"; // Import the new TopSection component
 import { BottomNavigationTemplate } from "../templates/BottomNavigationTemplate";
 
+type torophyType = {
+  icon: string;
+  description: string;
+};
 const StatsContainer = () => {
-  const [trophies, setTrophies] = useState([]);
+  const [trophies, setTrophies] = useState<torophyType[]>([]);
 
   // Fetch the trophy data from trophy.json
   useEffect(() => {
@@ -14,19 +28,45 @@ const StatsContainer = () => {
   }, []);
 
   return (
-    <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100vh" }}>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100vh",
+      }}
+    >
       <Container sx={{ pt: 3 }}>
         {/* Top Section */}
         <TopSection />
         <Stack sx={{ margin: "30px auto 0", width: "90%" }}>
           <Box sx={{ mb: 3 }}>
-            <Typography variant="h4" sx={{ mb: 4, fontWeight: "bold", textAlign: "left" }}>
-              <EmojiEventsIcon sx={{ fontSize: 40, mr: 2, verticalAlign: "bottom" }} />
+            <Typography
+              variant="h4"
+              sx={{ mb: 4, fontWeight: "bold", textAlign: "left" }}
+            >
+              <EmojiEventsIcon
+                sx={{ fontSize: 40, mr: 2, verticalAlign: "bottom" }}
+              />
               参加データ
             </Typography>
 
-            <Paper sx={{ m: "0 auto", p: 4, width: "90%", backgroundColor: "secondary.main", borderRadius: "16px" }}>
-              <Box sx={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+            <Paper
+              sx={{
+                m: "0 auto",
+                p: 4,
+                width: "90%",
+                backgroundColor: "secondary.main",
+                borderRadius: "16px",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
                 <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
                   <Typography variant="h4" sx={{ color: "#FF007F" }}>
                     5回
